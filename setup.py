fba04d33700dd5ead09d3c562a5c743a029c0319
言葉 <funaox@gmail.com>
2022-08-25 09:39:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shell2udp',
-    version='1.1',
+    version='1.2',
     description='Executing shell commands via UDP server',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
